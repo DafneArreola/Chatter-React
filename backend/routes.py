f5b66a4cf3752e28a96ec2f8f990b7244b42f3a4
Dafne Arreola <129788533+DafneArreola@users.noreply.github.com>
2024-07-19 06:10:52 +0000

    fixed a bug in routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, jsonify, render_template
-from backend.models import User, Media, Favorite, Rating, Comment
 from backend.database import db
 from backend.movie_api import get_movie_details, search_movies, get_popular_movies
 
