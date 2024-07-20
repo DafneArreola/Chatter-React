0d8e8ae2534eaebe4de61df750f04ae1c60e5621
Dafne Arreola <129788533+DafneArreola@users.noreply.github.com>
2024-07-20 05:45:40 +0000

    added search pages for shows and movies

--- frontend/static/js/home.js
+++ frontend/static/js/home.js
@@ -2,5 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     $('#trendingMoviesCarousel').carousel({
         interval: 20000
     });
-    
+    $('#trendingShowsCarousel').carousel({
+        interval: 20000
+    });
+    $('#trendingMusicCarousel').carousel({
+        interval: 20000
+    });
 });
