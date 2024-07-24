9a11d8e93052f35c4c0f80e282fdad17fc8b6332
Edder Hernandez <eddeh17963@gmail.com>
2024-07-24 11:23:26 -0500

    revert

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     
-    SQLALCHEMY_DATABASE_URI = '/instance' #os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
